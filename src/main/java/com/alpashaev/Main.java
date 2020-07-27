85c1c17d0c4db2097d940c547e621d919b3b42c9
Elementrixi <67051385+Elementrixi@users.noreply.github.com>
2020-07-27 20:45:56 +0300

    init commit

---
+++ src/main/java/com/alpashaev/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.alpashaev;
+
+import executor.Controller;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Controller.homeworkSolution();
+    }
+}
