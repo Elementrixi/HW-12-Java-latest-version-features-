f929ae48a759e24670db9c841f173c0218d6c828
Elementrixi <67051385+Elementrixi@users.noreply.github.com>
2020-07-30 14:29:24 +0300

    added edit user opportunity

--- custom.module/src/service/Service.java
+++ custom.module/src/service/Service.java
@@ -4,8 +4,10 @@ import user.User;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Service {
+    static Scanner scr = new Scanner(System.in);
     private static Map<Integer, User> userMap = new HashMap<>();
     private static int userId = 1;
 
@@ -18,6 +20,20 @@ public class Service {
     }
 
     public static void editUser() {
+        Service.userList();
+        System.out.println("Enter id of user who you will edit");
+        int userId = scr.nextInt();
+        System.out.println("Edited name : ");
+        String name = scr.next();
+
+        System.out.println("Edited country: ");
+        String country = scr.next();
+
+        System.out.println("Edited age: ");
+        int age = Integer.parseInt(scr.next());
+        Service.deleteUser(userId);
+        Service.addUser(name, country, age);
+        System.out.println("Account updated");
     }
 
     public static void userList() {
